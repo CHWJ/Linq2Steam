d76e9e85a231fbf870ac221344102f9a7fe95d55
CHWJ <jwhc2012@gmail.com>
2020-03-25 22:51:06 +0800

    add c#: Aggregate

---
+++ LINQSamples/Aggregate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQSamples
+{
+    class Aggregate
+    {
+        public static void Run()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            var result = numbers.Aggregate((a, b) => a + b);
+            Console.WriteLine(result);
+
+            result = numbers.Aggregate((a, b) => a * b);
+            Console.WriteLine(result);
+        }
+    }
+}
