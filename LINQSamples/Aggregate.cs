2723b2ebce3a76f11c807eb3cdd0ec534bd1ee0e
CHWJ <jwhc2012@gmail.com>
2020-04-25 16:18:40 +0800

    update Aggregate

--- LINQSamples/Aggregate.cs
+++ LINQSamples/Aggregate.cs
@@ -5,14 +5,75 @@ namespace LINQSamples
 {
     class Aggregate
     {
+        private static readonly int[] Numbers = new int[] {1, 2, 3, 4, 5};
+
         public static void Run()
         {
-            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            SampleAggregateLambdaSimple();
 
-            var result = numbers.Aggregate((a, b) => a + b);
+            SampleAggregateLambdaSeed();
+
+            SampleAverageLambda();
+
+            SampleCountLambda();
+
+            SampleMaxLambda();
+
+            SampleMinLambda();
+
+            SampleSumLambda();
+        }
+
+        private static void SampleAggregateLambdaSimple()
+        {
+            var result = Numbers.Aggregate((a, b) => a + b);
+            // 15
             Console.WriteLine(result);
 
-            result = numbers.Aggregate((a, b) => a * b);
+            result = Numbers.Aggregate((a, b) => a * b);
+            // 120
+            Console.WriteLine(result);
+        }
+
+        private static void SampleAggregateLambdaSeed()
+        {
+            var result = Numbers.Aggregate(10, (a, b) => a + b);
+            // 25
+            Console.WriteLine(result);
+        }
+
+        private static void SampleAverageLambda()
+        {
+            var result = Numbers.Average();
+            // 3
+            Console.WriteLine(result);
+        }
+
+        private static void SampleCountLambda()
+        {
+            var result = Numbers.Count();
+            // 5
+            Console.WriteLine(result);
+        }
+
+        private static void SampleMaxLambda()
+        {
+            var result = Numbers.Max();
+            // 5
+            Console.WriteLine(result);
+        }
+
+        private static void SampleMinLambda()
+        {
+            var result = Numbers.Min();
+            // 1
+            Console.WriteLine(result);
+        }
+
+        private static void SampleSumLambda()
+        {
+            var result = Numbers.Sum();
+            // 15
             Console.WriteLine(result);
         }
     }
